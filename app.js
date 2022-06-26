9d8965e22d7e30702b6b486cac508301fe153826
samuel cordova <cordovas1337@gmail.com>
2022-06-26 13:33:42 -0500

    added code to check if the square clicked was the one with the mole, if it was add a point

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
     // select all squares so we can work with them in JS
-    const squares = document.querySelectorAll('.square');
+    const squares = document.querySelectorAll('.square')
     // grab the mole 
-    const mole = document.querySelector('.mole');
+    const mole = document.querySelector('.mole')
     // grab the timer
     const timeLeft = document.querySelector('#time-left')
     // grab the score
-    const score = document.querySelector('#score');
+    const score = document.querySelector('#score')
 
     let result = 0
+    let hitPosition
 
     // function to select a random square for the mole
     function randomSquare() {
@@ -22,10 +23,29 @@
         // add the class mole to the random square so that the mole will appear there.
         randomSquare.classList.add('mole')
        
-        // console.log(randomPosition)
+        // using hitPosition to identify which square was clicked by ID
+        hitPosition = randomSquare.id
+
     }
 
 // need to find a way to check if a mole was clicked and if so add a point to the score.
+
+
+squares.forEach(square => {
+    // for each sqaure listen for a mousedown event and run this function everytime we click on the square.
+square.addEventListener('mousedown', () => {
+    // if square clicked is ecqual to the position of the mole.
+if (square.id == hitPosition) {
+    // add point to the score using result.
+    result++
+    console.log('result')
+    // set score text content to be the same as 'result'
+    score.textContent = result
+    // clear out hit position after adding a point so that we can add another point if we hit another mole.
+    hitPosition = null
+        }
+    })
+})
 
     // A fucntion to run the randomSquare function every so often which in turn will move the 'mole'.
     // Can attach the moveMole function to button to start the game.
